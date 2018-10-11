15ffe383ada2ee2a221f798d0db3dfa3c57e161d
Petter Kraabøl <petter.zarlach@gmail.com>
2018-10-12 01:13:18 +0200

    Fixed Twitch init file.

--- setup.py
+++ setup.py
@@ -34,6 +34,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/PetterKraabol/Twitch-Python',
-    version='0.0.3',
+    version='0.0.4',
     zip_safe=True,
 )
--- twitch/__init__.py
+++ twitch/__init__.py
@@ -1,1 +1,4 @@
 name = "twitch"
+from .helix import Helix
+from .v5 import V5
+from .chat import Chat
