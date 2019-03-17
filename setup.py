24785d7f21597ad018eb1b6391ba505521210164
Petter Kraabøl <petter.zarlach@gmail.com>
2019-03-17 03:13:44 +0100

    Updated version number.

--- setup.py
+++ setup.py
@@ -39,6 +39,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/PetterKraabol/Twitch-Python',
-    version='0.0.9',
+    version='0.0.10',
     zip_safe=True,
 )
