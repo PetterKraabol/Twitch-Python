104c0bc0c8dbb3a1292a45a4d0838cc8222aba82
Petter Kraabøl <petter.zarlach@gmail.com>
2019-08-28 18:39:20 +0200

    Code cleanup for tests.

--- tests/test_helix_user.py
+++ tests/test_helix_user.py
@@ -1,5 +1,6 @@
 import tracemalloc
 import unittest
+from typing import Dict, Any
 
 import responses
 
@@ -9,24 +10,49 @@ tracemalloc.start()
 
 
 class TestHelixUser(unittest.TestCase):
+    user: Dict[str, Dict[str, Any]] = {
+        'zarlach': {
+            'id': '24250859',
+            'login': 'zarlach',
+            'display_name': 'Zarlach',
+            'type': '',
+            'broadcaster_type': '',
+            'description': '',
+            'profile_image_url': 'https://static-cdn.jtvnw.net/jtv_user_pictures/zarlach-profile_image-1cb98e7eadb5918a-300x300.png',
+            'offline_image_url': 'https://static-cdn.jtvnw.net/jtv_user_pictures/zarlach-channel_offline_image-f2d036ac9582d793-1920x1080.png',
+            'view_count': 1664
+        },
+        'sodapoppin': {
+            'id': '26301881',
+            'login': 'sodapoppin',
+            'display_name': 'sodapoppin',
+            'type': '',
+            'broadcaster_type': 'partner',
+            'description': 'Wtf do i write here? Click my stream, or i scream.',
+            'profile_image_url': 'https://static-cdn.jtvnw.net/jtv_user_pictures/sodapoppin-profile_image-10049b6200f90c14-300x300.png',
+            'offline_image_url': 'https://static-cdn.jtvnw.net/jtv_user_pictures/7ed72b04-897e-4a85-a3c9-2a8ba74aeaa7-channel_offline_image-1920x1080.jpg',
+            'view_count': 276164750
+        }
+    }
+
+    def setUp(self) -> None:
+        responses.add(responses.GET, 'https://api.twitch.tv/helix/users?login=zarlach',
+                      match_querystring=True,
+                      json={'data': [TestHelixUser.user['zarlach']]})
+        responses.add(responses.GET, 'https://api.twitch.tv/helix/users?login=sodapoppin',
+                      match_querystring=True,
+                      json={'data': [TestHelixUser.user['sodapoppin']]})
+
+        responses.add(responses.GET, 'https://api.twitch.tv/helix/users?id=24250859&login=sodapoppin',
+                      match_querystring=True,
+                      json={'data': [TestHelixUser.user['zarlach'], TestHelixUser.user['sodapoppin']]})
+
+        responses.add(responses.GET, 'https://api.twitch.tv/helix/users?login=sodapoppin&login=zarlach',
+                      match_querystring=True,
+                      json={'data': [TestHelixUser.user['zarlach'], TestHelixUser.user['sodapoppin']]})
 
     @responses.activate
     def test_user(self):
-        responses.add(responses.GET, 'https://api.twitch.tv/helix/users?login=zarlach',
-                      match_querystring=True,
-                      json={
-                          'data': [{'id': '24250859',
-                                    'login': 'zarlach',
-                                    'display_name': 'Zarlach',
-                                    'type': '',
-                                    'broadcaster_type': '',
-                                    'description': '',
-                                    'profile_image_url': 'https://static-cdn.jtvnw.net/jtv_user_pictures/zarlach-profile_image-1cb98e7eadb5918a-300x300.png',
-                                    'offline_image_url': 'https://static-cdn.jtvnw.net/jtv_user_pictures/zarlach-channel_offline_image-f2d036ac9582d793-1920x1080.png',
-
-                                    'view_count': 1664}]
-                      })
-
         helix = twitch.Helix('client-id', use_cache=True)
 
         # Get display name to display name
@@ -34,47 +60,6 @@ class TestHelixUser(unittest.TestCase):
 
     @responses.activate
     def test_users(self):
-        responses.add(responses.GET, 'https://api.twitch.tv/helix/users?login=sodapoppin',
-                      match_querystring=True,
-                      json={
-                          'data': [
-                              {'id': '26301881',
-                               'login': 'sodapoppin',
-                               'display_name': 'sodapoppin',
-                               'type': '',
-                               'broadcaster_type': 'partner',
-                               'description': 'Wtf do i write here? Click my stream, or i scream.',
-                               'profile_image_url': 'https://static-cdn.jtvnw.net/jtv_user_pictures/sodapoppin-profile_image-10049b6200f90c14-300x300.png',
-                               'offline_image_url': 'https://static-cdn.jtvnw.net/jtv_user_pictures/7ed72b04-897e-4a85-a3c9-2a8ba74aeaa7-channel_offline_image-1920x1080.jpg',
-                               'view_count': 276164750}]
-                      }
-                      )
-
-        responses.add(responses.GET, 'https://api.twitch.tv/helix/users?id=24250859&login=sodapoppin',
-                      match_querystring=True,
-                      json={
-                          'data': [
-                              {'id': '24250859',
-                               'login': 'zarlach',
-                               'display_name': 'Zarlach',
-                               'type': '',
-                               'broadcaster_type': '',
-                               'description': '',
-                               'profile_image_url': 'https://static-cdn.jtvnw.net/jtv_user_pictures/zarlach-profile_image-1cb98e7eadb5918a-300x300.png',
-                               'offline_image_url': 'https://static-cdn.jtvnw.net/jtv_user_pictures/zarlach-channel_offline_image-f2d036ac9582d793-1920x1080.png',
-
-                               'view_count': 1664},
-                              {'id': '26301881',
-                               'login': 'sodapoppin',
-                               'display_name': 'sodapoppin',
-                               'type': '',
-                               'broadcaster_type': 'partner',
-                               'description': 'Wtf do i write here? Click my stream, or i scream.',
-                               'profile_image_url': 'https://static-cdn.jtvnw.net/jtv_user_pictures/sodapoppin-profile_image-10049b6200f90c14-300x300.png',
-                               'offline_image_url': 'https://static-cdn.jtvnw.net/jtv_user_pictures/7ed72b04-897e-4a85-a3c9-2a8ba74aeaa7-channel_offline_image-1920x1080.jpg',
-                               'view_count': 276164750}]
-                      })
-
         # Should returned cached data from above
         helix = twitch.Helix('client-id', use_cache=True)
 
@@ -83,31 +68,6 @@ class TestHelixUser(unittest.TestCase):
 
     @responses.activate
     def test_custom_user_cache(self):
-        responses.add(responses.GET, 'https://api.twitch.tv/helix/users?login=sodapoppin&login=zarlach',
-                      match_querystring=True,
-                      json={
-                          'data': [
-                              {'id': '24250859',
-                               'login': 'zarlach',
-                               'display_name': 'Zarlach',
-                               'type': '',
-                               'broadcaster_type': '',
-                               'description': '',
-                               'profile_image_url': 'https://static-cdn.jtvnw.net/jtv_user_pictures/zarlach-profile_image-1cb98e7eadb5918a-300x300.png',
-                               'offline_image_url': 'https://static-cdn.jtvnw.net/jtv_user_pictures/zarlach-channel_offline_image-f2d036ac9582d793-1920x1080.png',
-                               'view_count': 1664},
-                              {'id': '26301881',
-                               'login': 'sodapoppin',
-                               'display_name': 'sodapoppin',
-                               'type': '',
-                               'broadcaster_type': 'partner',
-                               'description': 'Wtf do i write here? Click my stream, or i scream.',
-                               'profile_image_url': 'https://static-cdn.jtvnw.net/jtv_user_pictures/sodapoppin-profile_image-10049b6200f90c14-300x300.png',
-                               'offline_image_url': 'https://static-cdn.jtvnw.net/jtv_user_pictures/7ed72b04-897e-4a85-a3c9-2a8ba74aeaa7-channel_offline_image-1920x1080.jpg',
-                               'view_count': 276164750}]
-                      }
-                      )
-
         helix = twitch.Helix('client-id', use_cache=True)
         helix.users(['zarlach', 'sodapoppin'])
 
--- tests/test_helix_video.py
+++ tests/test_helix_video.py
@@ -1,38 +1,70 @@
 import tracemalloc
 import unittest
+from typing import Dict, Any
 
 import responses
 
 import twitch
+from .test_helix_user import TestHelixUser
 
 tracemalloc.start()
 
 
 class TestHelixVideo(unittest.TestCase):
+    video: Dict[str, Dict[str, Any]] = {
+        '471855782': {
+            'id': '471855782',
+            'user_id': '26301881',
+            'user_name': 'sodapoppin',
+            'title': '2 days til Classic, passing the time til then. ',
+            'description': '',
+            'created_at': '2019-08-24T21:34:03Z',
+            'published_at': '2019-08-24T21:34:03Z',
+            'url': 'https://www.twitch.tv/videos/471855782',
+            'thumbnail_url': '',
+            'viewable': 'public',
+            'view_count': 329,
+            'language': 'en',
+            'type': 'archive',
+            'duration': '2h14m55s'
+        },
+        '471295896': {
+            'id': '471295896',
+            'user_id': '26301881',
+            'user_name': 'sodapoppin',
+            'title': '3 days til Classic, passing the time til then. ',
+            'description': '',
+            'created_at': '2019-08-23T18:40:05Z',
+            'published_at': '2019-08-23T18:40:05Z',
+            'url': 'https://www.twitch.tv/videos/471295896',
+            'thumbnail_url': 'https://static-cdn.jtvnw.net/s3_vods/7d5ae2c2918cf4ca8579_sodapoppin_35403289856_1281380185/thumb/thumb0-%{width}x%{height}.jpg',
+            'viewable': 'public',
+            'view_count': 5892,
+            'language': 'en',
+            'type': 'archive',
+            'duration': '6h4m13s'}
+    }
 
-    @responses.activate
-    def test_video(self):
+    def setUp(self) -> None:
         responses.add(responses.GET, 'https://api.twitch.tv/helix/videos?id=471855782',
                       match_querystring=True,
                       json={
-                          'data': [{
-                              'id': '471855782',
-                              'user_id': '26301881',
-                              'user_name': 'sodapoppin',
-                              'title': '2 days til Classic, passing the time til then. ',
-                              'description': '',
-                              'created_at': '2019-08-24T21:34:03Z',
-                              'published_at': '2019-08-24T21:34:03Z',
-                              'url': 'https://www.twitch.tv/videos/471855782',
-                              'thumbnail_url': '',
-                              'viewable': 'public',
-                              'view_count': 329,
-                              'language': 'en',
-                              'type': 'archive',
-                              'duration': '2h14m55s'}],
+                          'data': [TestHelixVideo.video['471855782']],
                           'pagination': {'cursor': 'eyJiIjpudWxsLCJhIjp7Ik9mZnNldCI6MX19'}
                       })
+        responses.add(responses.GET, 'https://api.twitch.tv/helix/users?login=sodapoppin',
+                      match_querystring=True,
+                      json={'data': [TestHelixUser.user['sodapoppin']]})
 
+        responses.add(responses.GET, 'https://api.twitch.tv/helix/videos?user_id=26301881&first=2',
+                      match_querystring=True,
+                      json={
+                          'data': [TestHelixVideo.video['471855782'], TestHelixVideo.video['471295896']],
+                          'pagination': {'cursor': 'eyJiIjpudWxsLCJhIjp7Ik9mZnNldCI6Mn19'}
+                      })
+
+    @responses.activate
+    def test_video(self):
         helix = twitch.Helix('client-id', use_cache=True)
 
         # Get display name to display name
@@ -40,43 +72,6 @@ class TestHelixVideo(unittest.TestCase):
 
     @responses.activate
     def test_first_videos(self):
-        responses.add(responses.GET, 'https://api.twitch.tv/helix/users?login=sodapoppin',
-                      match_querystring=True,
-                      json={
-                          'data': [
-                              {
-                                  'id': '26301881', 'login': 'sodapoppin', 'display_name': 'sodapoppin', 'type': '',
-                                  'broadcaster_type': 'partner',
-                                  'description': 'Wtf do i write here? Click my stream, or i scream.',
-                                  'profile_image_url': 'https://static-cdn.jtvnw.net/jtv_user_pictures/sodapoppin-profile_image-10049b6200f90c14-300x300.png',
-                                  'offline_image_url': 'https://static-cdn.jtvnw.net/jtv_user_pictures/7ed72b04-897e-4a85-a3c9-2a8ba74aeaa7-channel_offline_image-1920x1080.jpg',
-                                  'view_count': 287171213
-                              }]
-                      })
-
-        responses.add(responses.GET, 'https://api.twitch.tv/helix/videos?user_id=26301881&first=2',
-                      match_querystring=True,
-                      json={
-                          'data': [
-                              {
-                                  'id': '471855782', 'user_id': '26301881', 'user_name': 'sodapoppin',
-                                  'title': '2 days til Classic, passing the time til then. ', 'description': '',
-                                  'created_at': '2019-08-24T21:34:03Z', 'published_at': '2019-08-24T21:34:03Z',
-                                  'url': 'https://www.twitch.tv/videos/471855782', 'thumbnail_url': '',
-                                  'viewable': 'public', 'view_count': 453, 'language': 'en', 'type': 'archive',
-                                  'duration': '2h21m43s'
-                              },
-                              {
-                                  'id': '471295896', 'user_id': '26301881', 'user_name': 'sodapoppin',
-                                  'title': '3 days til Classic, passing the time til then. ', 'description': '',
-                                  'created_at': '2019-08-23T18:40:05Z', 'published_at': '2019-08-23T18:40:05Z',
-                                  'url': 'https://www.twitch.tv/videos/471295896',
-                                  'thumbnail_url': 'https://static-cdn.jtvnw.net/s3_vods/7d5ae2c2918cf4ca8579_sodapoppin_35403289856_1281380185/thumb/thumb0-%{width}x%{height}.jpg',
-                                  'viewable': 'public', 'view_count': 5892, 'language': 'en', 'type': 'archive',
-                                  'duration': '6h4m13s'}],
-                          'pagination': {'cursor': 'eyJiIjpudWxsLCJhIjp7Ik9mZnNldCI6Mn19'}
-                      })
-
         # Should returned cached data from above
         helix = twitch.Helix('client-id', use_cache=True)
 
@@ -85,28 +80,8 @@ class TestHelixVideo(unittest.TestCase):
 
     @responses.activate
     def test_custom_video_cache(self):
-        responses.add(responses.GET, 'https://api.twitch.tv/helix/videos?id=471855782',
-                      match_querystring=True,
-                      json={
-                          'data': [
-                              {'id': '471855782',
-                               'user_id': '26301881',
-                               'user_name': 'sodapoppin',
-                               'title': '2 days til Classic, passing the time til then. ',
-                               'description': '',
-                               'created_at': '2019-08-24T21:34:03Z',
-                               'published_at': '2019-08-24T21:34:03Z',
-                               'url': 'https://www.twitch.tv/videos/471855782',
-                               'thumbnail_url': '',
-                               'viewable': 'public',
-                               'view_count': 329,
-                               'language': 'en',
-                               'type': 'archive',
-                               'duration': '2h14m55s'}],
-                          'pagination': {'cursor': 'eyJiIjpudWxsLCJhIjp7Ik9mZnNldCI6MX19'}})
-
         helix = twitch.Helix('client-id', use_cache=True)
-        helix.video(471855782)
+        _ = helix.video(471855782)
 
         # Videos have custom caching, such that url should not be cached
         self.assertFalse(helix.api.SHARED_CACHE.has('GET:https://api.twitch.tv/helix/videos?id=471855782'))
