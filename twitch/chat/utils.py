4bc842f80767f5abb888b12b694ad697ce99cdb1
RduMarais <rmichon@telecom-paristech.fr>
2022-12-22 11:11:46 +0100

    added attribute in irc instead of exception

--- twitch/chat/utils.py
+++
@@ -1,7 +0,0 @@
-
-
-class TwitchChatError(Exception):
-	
-	def __init__(self,message):
-		self.message = message
-		super().__init__(message)
---
+++ twitch/utils.py
@@ -0,0 +1,12 @@
+
+
+class TwitchChatError(Exception):
+	
+	def __init__(self,message):
+		super().__init__(message)
+
+print('test')
+
+
+def print_test():
+	print('import test')
